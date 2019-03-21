efb395d5e3b8272f752861064da9969688f74737
Łukasz Wójcik <lwojcik@lukem.net>
2019-03-21 23:01:16 +0100

    add project files

---
+++ src/index.ts
@@ -0,0 +1,93 @@
+import { FastifyInstance } from 'fastify';
+import fp from 'fastify-plugin';
+
+import TwitchEbsTools, {
+  TwitchToken,
+  TwitchPayload,
+  TwitchChannelId,
+  TwitchRole,
+  TwitchRoles,
+} from '../../twitch-ebs-tools/src/index';
+
+export interface PluginOptions {
+  readonly secret: string;
+  readonly disabled: boolean;
+}
+
+function fastifyTwitchEbsTools(
+  fastify: FastifyInstance,
+  options: PluginOptions,
+  next: CallableFunction,
+): void {
+  if (!options.secret) {
+    return next(new Error('Twitch EBS Tools: Missing JWT secret'));
+  }
+
+  const { secret } = options;
+  const disabled = options.disabled || false;
+
+  function validateToken(token: TwitchToken): TwitchPayload | Error {
+    return new TwitchEbsTools(secret).validateToken(token);
+  }
+
+  function verifyChannelId(payload: TwitchPayload, channelId: TwitchChannelId): boolean {
+    if (disabled) return true;
+    return TwitchEbsTools.verifyChannelId(payload, channelId);
+  }
+
+  function verifyTokenNotExpired(payload: TwitchPayload): boolean {
+    if (disabled) return true;
+    return TwitchEbsTools.verifyTokenNotExpired(payload);
+  }
+
+  function verifyRole(payload: TwitchPayload, role: TwitchRole): boolean {
+    if (disabled) return true;
+    return TwitchEbsTools.verifyRole(payload, role);
+  }
+
+  function verifyChannelIdAndRole(
+    payload: TwitchPayload,
+    channelId: TwitchChannelId,
+    role: TwitchRole,
+  ): boolean {
+    if (disabled) return true;
+    return verifyChannelIdAndRole(payload, channelId, role);
+  }
+
+  function verifyBroadcaster(payload: TwitchPayload): boolean {
+    if (disabled) return true;
+    return TwitchEbsTools.verifyBroadcaster(payload);
+  }
+
+  function verifyViewerOrBroadcaster(payload: TwitchPayload): boolean {
+    if (disabled) return true;
+    return TwitchEbsTools.verifyViewerOrBroadcaster(payload);
+  }
+
+  function validatePermission(
+    token: TwitchToken,
+    channelId: TwitchChannelId,
+    roles: TwitchRole | TwitchRoles,
+  ): boolean {
+    if (disabled) return true;
+    return new TwitchEbsTools(secret).validatePermission(token, channelId, roles);
+  }
+
+  fastify.decorate('twitchEbsTools', {
+    validateToken,
+    validatePermission,
+    verifyTokenNotExpired,
+    verifyBroadcaster,
+    verifyChannelId,
+    verifyRole,
+    verifyViewerOrBroadcaster,
+    verifyChannelIdAndRole,
+  });
+
+  next();
+}
+
+export default fp(fastifyTwitchEbsTools, {
+  fastify: '>=2.0.0',
+  name: 'fastify-twitch-ebs-tools',
+});
